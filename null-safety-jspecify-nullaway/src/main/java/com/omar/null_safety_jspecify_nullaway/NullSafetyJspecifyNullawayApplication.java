84c053fc363adf4223320d09823c6be33b065612
DrissiOmar <omar.drissi@esprit.tn>
2025-12-22 15:58:59 +0100

    chore: initialize Spring Boot 4 project for null-safety with JSpecify and NullAway

---
+++ null-safety-jspecify-nullaway/src/main/java/com/omar/null_safety_jspecify_nullaway/NullSafetyJspecifyNullawayApplication.java
@@ -0,0 +1,13 @@
+package com.omar.null_safety_jspecify_nullaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NullSafetyJspecifyNullawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NullSafetyJspecifyNullawayApplication.class, args);
+	}
+
+}
---
+++ null-safety-jspecify-nullaway/src/test/java/com/omar/null_safety_jspecify_nullaway/NullSafetyJspecifyNullawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.null_safety_jspecify_nullaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullSafetyJspecifyNullawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

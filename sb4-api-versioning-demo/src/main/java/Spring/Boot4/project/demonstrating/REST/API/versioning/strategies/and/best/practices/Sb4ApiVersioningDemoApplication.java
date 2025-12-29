f44ecf28c83fa84d9f44e88b6ae800c2a4d25b48
DrissiOmar <omar.drissi@esprit.tn>
2025-12-29 18:11:25 +0100

    chore: initialize Spring Boot 4 API versioning demo project

---
+++ sb4-api-versioning-demo/src/main/java/Spring/Boot4/project/demonstrating/REST/API/versioning/strategies/and/best/practices/Sb4ApiVersioningDemoApplication.java
@@ -0,0 +1,13 @@
+package Spring.Boot4.project.demonstrating.REST.API.versioning.strategies.and.best.practices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb4ApiVersioningDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb4ApiVersioningDemoApplication.class, args);
+	}
+
+}
---
+++ sb4-api-versioning-demo/src/test/java/Spring/Boot4/project/demonstrating/REST/API/versioning/strategies/and/best/practices/Sb4ApiVersioningDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot4.project.demonstrating.REST.API.versioning.strategies.and.best.practices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb4ApiVersioningDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

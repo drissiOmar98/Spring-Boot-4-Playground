12b510687f5d765acb6bded632a5d98840a2dfab
DrissiOmar <omar.drissi@esprit.tn>
2025-12-29 22:00:48 +0100

    chore: initialize Spring Boot 4 path-segment API versioning demo project

---
+++ api-versioning-path-segment/src/main/java/com/omar/api_versioning_path_segment/ApiVersioningPathSegmentApplication.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_path_segment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersioningPathSegmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVersioningPathSegmentApplication.class, args);
+	}
+
+}
---
+++ api-versioning-path-segment/src/test/java/com/omar/api_versioning_path_segment/ApiVersioningPathSegmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_path_segment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningPathSegmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

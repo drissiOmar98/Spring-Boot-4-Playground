ab4049e8aa747868feb62a708fc53ac981a552c4
DrissiOmar <omar.drissi@esprit.tn>
2025-12-29 20:08:25 +0100

    feat(api-versioning): configure flexible API versioning with path, header, query, and media-type support using a custom version parser

---
+++ api-versioning-demo/src/main/java/com/omar/api_versioning_demo/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.omar.api_versioning_demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureApiVersioning(ApiVersionConfigurer configurer) {
+        configurer
+                .usePathSegment(1)
+                .addSupportedVersions("1.0","2.0")
+                .setDefaultVersion("1.0")
+                //.useRequestHeader("X-API-Version")
+                //.useQueryParam("version")
+                //.useMediaTypeParameter(MediaType.APPLICATION_JSON, "version")
+                .setVersionParser(new ApiVersionParser());
+    }
+}

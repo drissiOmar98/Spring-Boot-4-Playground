9e567e3b7843e0db82c01597ac3fb5fcf5c08be8
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 19:52:41 +0100

    test(config): configure RestTestClient to use API-Version header for versioning

---
+++ api-versioning-header/src/test/java/com/omar/api_versioning_header/RestTestClientConfig.java
@@ -0,0 +1,15 @@
+package com.omar.api_versioning_header;
+
+import org.springframework.boot.resttestclient.autoconfigure.RestTestClientBuilderCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.web.client.ApiVersionInserter;
+
+@TestConfiguration
+public class RestTestClientConfig implements RestTestClientBuilderCustomizer {
+    @Override
+    public void customize(RestTestClient.Builder<?> builder) {
+
+        builder.apiVersionInserter(ApiVersionInserter.useHeader("API-Version"));
+    }
+}

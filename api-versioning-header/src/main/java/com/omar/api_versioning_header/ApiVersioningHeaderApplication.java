4207e3f8b684ecc654ecb9f1bed33f540330d8fe
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 19:42:48 +0100

    chore: initialize Spring Boot 4 API versioning demo using headers

---
+++ api-versioning-header/src/main/java/com/omar/api_versioning_header/ApiVersioningHeaderApplication.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_header;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersioningHeaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVersioningHeaderApplication.class, args);
+	}
+
+}
---
+++ api-versioning-header/src/test/java/com/omar/api_versioning_header/ApiVersioningHeaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_header;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningHeaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

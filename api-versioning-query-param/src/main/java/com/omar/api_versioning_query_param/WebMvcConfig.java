a73cede4dba3307cbff44080b1aa9fa7433c006c
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 16:55:13 +0100

    feat(query-param): configure API versioning using query parameter with default and supported versions

---
+++ api-versioning-query-param/src/main/java/com/omar/api_versioning_query_param/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package com.omar.api_versioning_query_param;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureApiVersioning(ApiVersionConfigurer configurer) {
+        configurer
+                .addSupportedVersions("1.0", "2.0")
+                .setDefaultVersion("1.0")
+                .useQueryParam("api-version")
+        ;
+    }
+
+}

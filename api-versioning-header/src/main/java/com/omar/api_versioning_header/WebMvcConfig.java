32837b100b432d7f4bb50a1ce78edeedb4a2efb0
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 19:45:37 +0100

    feat(header): configure API versioning using custom request header with default and supported versions

---
+++ api-versioning-header/src/main/java/com/omar/api_versioning_header/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.omar.api_versioning_header;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureApiVersioning(ApiVersionConfigurer configurer) {
+        configurer
+                .addSupportedVersions("1.0", "2.0")
+                .setDefaultVersion("1.0")
+                .useRequestHeader("API-Version")
+        ;
+    }
+}

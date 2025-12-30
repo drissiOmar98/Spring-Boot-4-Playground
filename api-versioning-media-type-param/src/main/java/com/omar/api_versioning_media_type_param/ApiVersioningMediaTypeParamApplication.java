6bba923580cf11c52d4107c9ac8e93d750b0de54
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 19:57:59 +0100

    chore(init): create Spring Boot project for media-type parameter API versioning

---
+++ api-versioning-media-type-param/src/main/java/com/omar/api_versioning_media_type_param/ApiVersioningMediaTypeParamApplication.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_media_type_param;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersioningMediaTypeParamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVersioningMediaTypeParamApplication.class, args);
+	}
+
+}
---
+++ api-versioning-media-type-param/src/test/java/com/omar/api_versioning_media_type_param/ApiVersioningMediaTypeParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_media_type_param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningMediaTypeParamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

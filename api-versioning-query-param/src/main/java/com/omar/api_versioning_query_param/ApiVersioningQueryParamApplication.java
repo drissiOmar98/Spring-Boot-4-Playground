a8f580820115630f648b08d8463cbcd2f6964343
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 16:51:14 +0100

    chore: initialize Spring Boot 4 API versioning demo using query parameters

---
+++ api-versioning-query-param/src/main/java/com/omar/api_versioning_query_param/ApiVersioningQueryParamApplication.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_query_param;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVersioningQueryParamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVersioningQueryParamApplication.class, args);
+	}
+
+}
---
+++ api-versioning-query-param/src/test/java/com/omar/api_versioning_query_param/ApiVersioningQueryParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.api_versioning_query_param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVersioningQueryParamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0d92b22271c50b318e78cf491a8af0f27e2cb173
DrissiOmar <omar.drissi@esprit.tn>
2025-12-30 19:51:30 +0100

    test(config): add MockMvcTestConfig to inject API-Version header for versioned endpoint testing

---
+++ api-versioning-header/src/test/java/com/omar/api_versioning_header/MockMvcTestConfig.java
@@ -0,0 +1,14 @@
+package com.omar.api_versioning_header;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.MockMvcBuilderCustomizer;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
+import org.springframework.web.client.ApiVersionInserter;
+
+@TestConfiguration
+class MockMvcTestConfig implements MockMvcBuilderCustomizer {
+    @Override
+    public void customize(ConfigurableMockMvcBuilder<?> builder) {
+        builder.apiVersionInserter(ApiVersionInserter.useHeader("API-Version"));
+    }
+}

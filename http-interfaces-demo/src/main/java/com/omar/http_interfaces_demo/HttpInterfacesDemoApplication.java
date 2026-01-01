73d3aa59d2a8457829104a9ccfc2c2b4b8f92435
DrissiOmar <omar.drissi@esprit.tn>
2026-01-01 12:58:21 +0100

    chore: initialize Spring Boot 4 HTTP Interfaces demo project

---
+++ http-interfaces-demo/src/main/java/com/omar/http_interfaces_demo/HttpInterfacesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.omar.http_interfaces_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpInterfacesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpInterfacesDemoApplication.class, args);
+	}
+
+}
---
+++ http-interfaces-demo/src/test/java/com/omar/http_interfaces_demo/HttpInterfacesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.http_interfaces_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpInterfacesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

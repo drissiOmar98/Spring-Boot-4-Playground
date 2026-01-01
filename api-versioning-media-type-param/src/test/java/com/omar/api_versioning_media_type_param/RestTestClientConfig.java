83548d227a9cced75a1441146a45d6ba99798c5a
DrissiOmar <omar.drissi@esprit.tn>
2026-01-01 12:37:48 +0100

    test(api-versioning): configure RestTestClient for media-type parameter versioning

---
+++ api-versioning-media-type-param/src/test/java/com/omar/api_versioning_media_type_param/RestTestClientConfig.java
@@ -0,0 +1,14 @@
+package com.omar.api_versioning_media_type_param;
+
+import org.springframework.boot.resttestclient.autoconfigure.RestTestClientBuilderCustomizer;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.web.servlet.client.RestTestClient;
+import org.springframework.web.client.ApiVersionInserter;
+
+@TestConfiguration
+public class RestTestClientConfig implements RestTestClientBuilderCustomizer {
+    @Override
+    public void customize(RestTestClient.Builder<?> builder) {
+        builder.apiVersionInserter(ApiVersionInserter.useMediaTypeParam("ver"));
+    }
+}

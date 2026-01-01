9b75910eab40d5213bf8e30430c7f0a2c5970878
DrissiOmar <omar.drissi@esprit.tn>
2026-01-01 14:55:28 +0100

    feat(model): add Todo & Post record for REST API representation

---
+++ http-interfaces-demo/src/main/java/com/omar/http_interfaces_demo/post/Post.java
@@ -0,0 +1,5 @@
+package com.omar.http_interfaces_demo.post;
+
+public record Post(Integer id, Integer userId, String title, String body) {
+
+}
---
+++ http-interfaces-demo/src/main/java/com/omar/http_interfaces_demo/todo/Todo.java
@@ -0,0 +1,5 @@
+package com.omar.http_interfaces_demo.todo;
+
+public record Todo(Integer id, Integer userId, String title, boolean completed) {
+
+}

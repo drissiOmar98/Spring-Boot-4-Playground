4dfb705b892f0235a3a5cee68e5824f28ec39257
DrissiOmar <omar.drissi@esprit.tn>
2026-01-02 14:37:44 +0100

    chore: bootstrap Spring Boot 4 project for RestTestClient API testing

---
+++ rest-test-client/src/main/java/com/omar/rest_test_client/RestTestClientApplication.java
@@ -0,0 +1,13 @@
+package com.omar.rest_test_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTestClientApplication.class, args);
+	}
+
+}
---
+++ rest-test-client/src/test/java/com/omar/rest_test_client/RestTestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.rest_test_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTestClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

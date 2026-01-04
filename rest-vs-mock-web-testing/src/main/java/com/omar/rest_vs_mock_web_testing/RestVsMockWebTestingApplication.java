90e78475c39af9276891beafa543aeacb4296762
DrissiOmar <omar.drissi@esprit.tn>
2026-01-04 21:01:05 +0100

    chore: initialize Spring Boot 4 testing playground comparing MockMvc and REST Test Client

---
+++ rest-vs-mock-web-testing/src/main/java/com/omar/rest_vs_mock_web_testing/RestVsMockWebTestingApplication.java
@@ -0,0 +1,13 @@
+package com.omar.rest_vs_mock_web_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVsMockWebTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVsMockWebTestingApplication.class, args);
+	}
+
+}
---
+++ rest-vs-mock-web-testing/src/test/java/com/omar/rest_vs_mock_web_testing/RestVsMockWebTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.rest_vs_mock_web_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVsMockWebTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

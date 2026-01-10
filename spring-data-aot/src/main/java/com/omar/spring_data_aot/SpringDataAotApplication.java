377c5e65cb756ca89ccf3fc416e6eefe432af760
DrissiOmar <omar.drissi@esprit.tn>
2026-01-10 20:27:07 +0100

    chore: bootstrap Spring Boot 4 project to explore Spring Data AOT repositories

---
+++ spring-data-aot/src/main/java/com/omar/spring_data_aot/SpringDataAotApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_data_aot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAotApplication.class, args);
+	}
+
+}

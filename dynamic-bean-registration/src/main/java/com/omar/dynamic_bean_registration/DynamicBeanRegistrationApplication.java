72e4fde564404154cf07e5bd69ad079931957cce
DrissiOmar <omar.drissi@esprit.tn>
2026-01-13 15:54:58 +0100

    chore(init): bootstrap Dynamic Bean Registration project with Spring Boot 4

---
+++ dynamic-bean-registration/src/main/java/com/omar/dynamic_bean_registration/DynamicBeanRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.omar.dynamic_bean_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicBeanRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicBeanRegistrationApplication.class, args);
+	}
+
+}
---
+++ dynamic-bean-registration/src/test/java/com/omar/dynamic_bean_registration/DynamicBeanRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.dynamic_bean_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicBeanRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a331cf75d936d3508474d04dcecb001b65e6b69c
DrissiOmar <omar.drissi@esprit.tn>
2026-01-23 14:09:45 +0100

    chore: initialize Spring Boot 4 observability project using OpenTelemetry starter

---
+++ opentelemetry-demo/src/main/java/com/omar/opentelemetry_demo/OpentelemetryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.omar.opentelemetry_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpentelemetryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpentelemetryDemoApplication.class, args);
+	}
+
+}

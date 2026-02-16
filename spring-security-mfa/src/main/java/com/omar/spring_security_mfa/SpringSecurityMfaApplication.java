9d9b1250a3557a4e26e67959419a4e91cd9e3bb8
DrissiOmar <omar.drissi@esprit.tn>
2026-02-16 15:41:06 +0100

    feat(MFA): initialize Spring Boot 4 project with Spring Security 7 MFA foundation

---
+++ spring-security-mfa/src/main/java/com/omar/spring_security_mfa/SpringSecurityMfaApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_security_mfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMfaApplication.class, args);
+	}
+
+}
---
+++ spring-security-mfa/src/test/java/com/omar/spring_security_mfa/SpringSecurityMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_security_mfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityMfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

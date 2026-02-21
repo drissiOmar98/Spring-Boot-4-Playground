b3546ea2b04bdcb75fe3c4c9350762ffdd5c7109
DrissiOmar <omar.drissi@esprit.tn>
2026-02-21 15:45:50 +0100

    feat(jackson3): bootstrap Jackson 3 JSON mapping module

---
+++ jackson3-json-mapping/src/main/java/com/omar/jackson3_json_mapping/Jackson3JsonMappingApplication.java
@@ -0,0 +1,13 @@
+package com.omar.jackson3_json_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jackson3JsonMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jackson3JsonMappingApplication.class, args);
+	}
+
+}
---
+++ jackson3-json-mapping/src/test/java/com/omar/jackson3_json_mapping/Jackson3JsonMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.jackson3_json_mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jackson3JsonMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

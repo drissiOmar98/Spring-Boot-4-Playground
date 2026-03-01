bff6a9aeb5ab672467660905f40fcd9dd60386f3
DrissiOmar <omar.drissi@esprit.tn>
2026-03-01 16:06:01 +0100

    feat(resilience): bootstrap spring-native-resilience project

---
+++ spring-native-resilience/src/main/java/com/omar/spring_native_resilience/SpringNativeResilienceApplication.java
@@ -0,0 +1,13 @@
+package com.omar.spring_native_resilience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeResilienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeResilienceApplication.class, args);
+	}
+
+}
---
+++ spring-native-resilience/src/test/java/com/omar/spring_native_resilience/SpringNativeResilienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.spring_native_resilience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNativeResilienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

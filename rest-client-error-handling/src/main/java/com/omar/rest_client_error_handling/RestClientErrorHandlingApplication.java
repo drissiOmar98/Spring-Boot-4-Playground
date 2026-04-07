800c468d8478248eea55124141c5e5a50a1aba34
DrissiOmar <omar.drissi@esprit.tn>
2026-04-07 18:57:21 +0100

    chore(rest-client-init): setup RestClient with centralized error handling, custom exceptions, and retry support

---
+++ rest-client-error-handling/src/main/java/com/omar/rest_client_error_handling/RestClientErrorHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.omar.rest_client_error_handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientErrorHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClientErrorHandlingApplication.class, args);
+	}
+
+}
---
+++ rest-client-error-handling/src/test/java/com/omar/rest_client_error_handling/RestClientErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.rest_client_error_handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientErrorHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

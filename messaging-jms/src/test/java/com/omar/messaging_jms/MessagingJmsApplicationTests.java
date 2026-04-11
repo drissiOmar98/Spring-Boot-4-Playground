b103405ebb413d5e523ccea02f772f2521774910
DrissiOmar <omar.drissi@esprit.tn>
2026-04-11 11:38:09 +0100

    feat(jms-client): initialize project with Spring Boot 4 and Apache ActiveMQ Artemis via Docker Compose

---
+++ messaging-jms/src/test/java/com/omar/messaging_jms/MessagingJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.messaging_jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
